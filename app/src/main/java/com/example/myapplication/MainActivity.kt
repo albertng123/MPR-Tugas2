394a292b7c8a2298372d135e018540e8e6c44f65
Albertng123 <albertbobby94@gmail.com>
2020-09-27 15:46:45 +0700

    Improve : setText Button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var valueA = Random.nextInt(0,100)
+    var valueB = Random.nextInt(0,100)
+    var valueC = Random.nextInt(0,100)
+    var valueD = Random.nextInt(0,100)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonE = findViewById<Button>(R.id.button1)
+        buttonE.setText("${valueA} + ${valueB}").toString()
 
-        var rand1 = Random.nextInt(0,100)
-        print(rand1)
-        button1.setText(rand1)
+        val buttonF = findViewById<Button>(R.id.button2)
+        buttonF.setText("${valueC} - ${valueD}").toString()
     }
 
-    //    val but1 = view.findViewById<Button>(R.id.button1)
-
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+    }
 }
